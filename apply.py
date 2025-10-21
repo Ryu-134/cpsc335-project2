7c59ed34765eb71e259b202e545837264e3fb605
Ryu-134 <caseydane134@gmail.com>
2025-10-20 17:09:45 -0700

    Initial environment created

---
+++ apply.py
@@ -0,0 +1,18 @@
+# Student Name: Casey Dane  
+# Titan Email: crdane@csu.fullerton.edu
+# Project: CPSC 335 – Interactive Campus Navigation System
+# Date: 10/24/2025
+
+
+import tkinter as tk
+
+
+def main():
+    root = tk.Tk()
+    root.title("CPSC 335 - Interactive Campus Navigation System (BFS/DFS)")
+    tk.Canvas(root, bg="white", width=800, height=600).pack(fill=tk.BOTH, expand=True)
+    root.mainloop()
+    
+    
+if __name__ == "__main__":
+    main()
